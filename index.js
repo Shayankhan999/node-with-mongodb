58ddd20e7aaa2655b195d42988d4393b29888948
Shayankhan <shayankhan9148@gmail.com>
2024-12-29 14:03:45 +0500

    get put post delete request have been updated today

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
-
+const tasks=[
+    {
+        id:1,
+        task:"sona nahe he shayan",
+    },
+    {
+        id:2,
+        task:"jagh na  he shayan",
+    },
+    {
+        id:3,
+        task:"concept samjha  he shayan",
+    },
+]
 
 import express  from "express";
 const app =express();
 const PORT = 4000;
 
 app.get('/',(req , res)=>{
-    // console.log(req)
-    res.send('hello world toI!' )
-}); 
+    res.status(200).send(tasks)
+});
 
+app.post('/',(req , res)=>{
+    res.send('Post request is called' )
+});
+
+app.put('/',(req , res)=>{
+    res.send('Put request is called' )
+});
+
+app.delete('/',(req , res)=>{
+    res.send('delete request is called' )
+});
 app.listen(PORT,()=>console.log("Server is running on PORT "+PORT))
